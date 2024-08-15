2683057f2d385b5c39a64b15bee75d8859ed204e
Sujen Shah <sujen1412@users.noreply.github.com>
2024-08-15 10:08:22 -0700

    Update run.sh to use conda run

--- dps/run.sh
+++ dps/run.sh
@@ -9,7 +9,5 @@ MODEL_NAME=$1
 START_YEAR=$2
 END_YEAR=$3
 
-source activate eis_model
-
-python $repo_root/model/main.py --model_name "${MODEL_NAME}" \
+conda run -n eis_model python $repo_root/model/main.py --model_name "${MODEL_NAME}" \
 --start_year "${START_YEAR}" --end_year "${END_YEAR}"
