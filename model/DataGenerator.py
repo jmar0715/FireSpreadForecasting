7a14db68601b658d44ce1b1fa47fbb655e2f75e6
jiannamar07 <jmm2469@columbia.edu>
2024-10-03 21:57:26 -0700

    main.py latest changes

--- model/DataGenerator.py
+++ model/DataGenerator.py
@@ -1,7 +1,65 @@
-from sklearn import preprocessing
 import fsspec
 import numpy as np
 import xarray as xr
+
+def data_processing(engine_type, feature_cols, fileObj, batch_size=1, lag=2):
+    all_data = []
+    with fsspec.open(fileObj, mode="rb") as f:
+        with xr.open_dataset(f, decode_coords='all', engine = engine_type, 
+                            cache = True, chunks = 'auto') as ds: #setting chunks as arbitrary
+
+            for col in feature_cols:
+                num_non_na = ds[col].count().values.sum()
+                if num_non_na == 0:
+                    input_data = ds[col].isel(channels=0).fillna(0)
+                else:
+                    input_data = ds[col].dropna(dim='channels', how='all')
+                    input_data = input_data.fillna(0).isel(channels=0)
+                
+                input_data = input_data.expand_dims(dim='channels', axis=-1)
+                input_data = input_data.transpose('x', 'y', 'time', 'channels')
+                
+                input_data = input_data.chunk({'x': input_data.sizes['x'], 
+                                               'y': input_data.sizes['y'], 
+                                               'time': input_data.sizes['time'], 
+                                               'channels': 1}) #1 chunk per input.. 5 input
+                
+                all_data.append(input_data)
+                
+            all_data_concat = xr.concat(all_data, dim='channels')
+            
+            all_data_concat = all_data_concat.chunk({'x': all_data_concat.sizes['x'], 
+                                                     'y': all_data_concat.sizes['y'], 
+                                                     'time': all_data_concat.sizes['time'],
+                                                    'channels': 5})
+            
+            all_data_concat = all_data_concat.compute()
+
+            time_len = all_data_concat.sizes['time']
+            max_idx = time_len - lag
+
+            for i in range(0, max_idx, batch_size):
+                if i + batch_size <= max_idx:
+                    batch_data = all_data_concat.isel(time = slice(i,i+batch_size))
+                else:
+                    batch_data = all_data_concat.isel(time = slice(i,max_idx))
+
+                if lag + batch_size + i <= time_len:
+                    target_data = all_data_concat.isel(time = slice(lag + i,lag + i + batch_size), channels=slice(-2,None))
+                else:
+                    target_data = all_data_concat.isel(time = slice(lag + i, time_len), channels = slice(-1,None))
+                
+                mean = np.mean(batch_data, axis=2, keepdims=True)
+                std = np.std(batch_data, axis=2, keepdims=True)
+                epsilon = 1e-8
+                std = np.where(std == 0, epsilon, std)
+                normalized_batch = (batch_data - mean) / std
+
+                # target_data = target_data.values
+
+                print(f"Yielding batch_data shape: {normalized_batch.shape}, target_data shape: {target_data.shape}")
+
+                yield normalized_batch, target_data
 
 
 def data_generator(file_list, batch_size, lag=2, feature_cols=None):
@@ -13,76 +71,13 @@ def data_generator(file_list, batch_size, lag=2, feature_cols=None):
         print(fileObj)
         try:
             print(f"Processing file: {fileObj}")
-            with fsspec.open(fileObj, mode="rb") as f:
-                ds = xr.open_dataset(f, decode_coords='all', engine='netcdf4')
-                ds.load()
-                for col in feature_cols:
-                    all_na = np.isnan(ds[col].values).all()
-                    if all_na == True:
-                        input_data = ds[col].transpose('x', 'y', 'time', 'channels')
-                        input_data = input_data.isel(channels=0).fillna(0)
-                    else:
-                        input_data = ds[col].dropna(dim='channels', how='all')
-                        input_data = input_data.transpose('x', 'y', 'time', 'channels')
-                        input_data = input_data.fillna(0).isel(channels=0)
-
-                    all_data.append(input_data)
-
-                all_data = xr.concat(all_data, dim='channels')
-                all_data = all_data.transpose('x', 'y', 'time', 'channels')
-                all_data = np.array(all_data)
+            yield from data_processing('h5netcdf', feature_cols, fileObj, batch_size, lag=2)
 
         except Exception as e1:
             print(e1)
             try:
-                with fsspec.open(fileObj, mode="rb") as f:
-                    ds = xr.open_dataset(f, decode_coords='all', engine='h5netcdf')
-                    ds.load()
-                    for col in feature_cols:
-                        all_na = np.isnan(ds[col].values).all()
-                        if all_na == True:
-                            input_data = ds[col].transpose('x', 'y', 'time', 'channels')
-                            input_data = input_data.isel(channels=0).fillna(0)
-                        else:
-                            input_data = ds[col].dropna(dim='channels', how='all')
-                            input_data = input_data.transpose('x', 'y', 'time', 'channels')
-                            input_data = input_data.fillna(0).isel(channels=0)
-
-                        all_data.append(input_data)
-
-                    all_data = xr.concat(all_data, dim='channels')
-
-                    all_data = all_data.transpose('x', 'y', 'time', 'channels')
-
-                    all_data = np.array(all_data)
+                yield from data_processing('netcdf4', feature_cols, fileObj, batch_size, lag=2)
 
             except Exception as e2:
                 print(e2)
-                # raise Exception(e2)
                 continue
-                
-        time_len = len(ds.time)
-        max_idx = time_len - lag
-
-        for i in range(0, max_idx, batch_size):
-            if i + batch_size <= max_idx:
-                batch_data = all_data[:, :, i:i + batch_size, :]
-            else:
-                batch_data = all_data[:, :, i:max_idx, :]
-
-            if lag + batch_size + i <= time_len:
-                target_data = all_data[:, :, lag + i:lag + i + batch_size, -2:]
-            else:
-                target_data = all_data[:, :, lag + i:time_len, -2:]
-
-            mean = np.mean(batch_data, axis=(0, 1, 2), keepdims=True)
-            std = np.std(batch_data, axis=(0, 1, 2), keepdims=True)
-            epsilon = 1e-8
-            std = np.where(std == 0, epsilon, std)
-            normalized_batch = (batch_data - mean) / std
-
-            # target_data = target_data.values
-
-            print(f"Yielding batch_data shape: {normalized_batch.shape}, target_data shape: {target_data.shape}")
-
-            yield normalized_batch, target_data
--- model/main.py
+++ model/main.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from DataGenerator import data_generator
+from DataGenerator import data_processing
 import argparse
 from model import define_model_architecture
 import fsspec
@@ -10,16 +11,21 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 
-def get_files(start='2014',end='2022'):
+def get_files(start,end, subset_number='10'): #10 fires sounds like reasonable starting point
     fs = fsspec.filesystem('s3', anon=True)
     s3 = s3fs.S3FileSystem(anon=False)
     all_files = []
-    years = range(int(start[-2:]),int(end[-2:]))
+    years = range(int(start[-2:]),int(end[-2:])) #extracting last two digits
+    subset_number = int(subset_number)
 
     for yr in years:
         file_paths_p = f"s3://maap-ops-workspace/shared/jiannamar07/wildfire_ids_{yr}/*_perimeter.nc"
-        print(file_paths_p)
         file_paths = s3.glob(file_paths_p)
+        
+        #subset only relevant if we are chunking for one year
+        if len(list(years)) == 1 or yr == list(years)[-1]: # < 1 or in between 1 complete and another
+            file_paths = file_paths[0:subset_number]
+            
         all_fire_ids = []
 
         for file in file_paths:
@@ -28,6 +34,7 @@ def get_files(start='2014',end='2022'):
 
         for fire in all_fire_ids:
             file_path = f"s3://maap-ops-workspace/shared/jiannamar07/wildfire_ids_{yr}/{fire}_all.nc"
+            print(file_path)
             all_files.append(file_path)
 
     return all_files
@@ -74,13 +81,14 @@ def main(args):
         tf.TensorSpec(shape=(None, None, 1, 5), dtype=tf.float32),
         tf.TensorSpec(shape=(None, None, 1, 2), dtype=tf.float32)
     )
-    all_files = get_files(args.start_year, args.end_year)
+    all_files = get_files(args.start_year, args.end_year) 
     
     np.random.seed(42)
     random.seed(42)
  
  
     train_data, validation_data = train_test_split(all_files, test_size = 0.25, random_state=42)
+    print('train data (also goes into as file list):', train_data)
  
 
     train_gen_ds, val_gen_ds = generate_train_and_test_data(train_data,
